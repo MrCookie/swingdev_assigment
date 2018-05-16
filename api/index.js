7931690ed058bb9da7f120cc4bc704925b623905
HubertRzepinski <hubert.rzepinski@gmail.com>
2018-05-16 22:27:39 +0100

    removed unnecessary api routes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,4 @@ const router = require('express').Router();
 
 router.use('/order', require('./order'));
 
-router.use('*', (req, res, next) => {
-	res.sendStatus(400);
-})
-
 module.exports = router;
