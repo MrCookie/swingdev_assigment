96477378d352aa38560d698f8a62baa14e801fdb
HubertRzepinski <hubert.rzepinski@gmail.com>
2018-05-17 11:09:14 +0100

    moved prices.pdf to pdf folder

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 var path = require('path');
 
 router.get('/prices', function (req, res, next) {
-  res.sendFile(path.join(__dirname + '/../public/prices.pdf'));
+  res.sendFile(path.join(__dirname + '/../public/pdf/prices.pdf'));
 });
 
 module.exports = router;
